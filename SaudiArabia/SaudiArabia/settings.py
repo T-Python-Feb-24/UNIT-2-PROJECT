82b36bf50ac3d8dbfee0d9c1fdcf857639d59df6
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-03-13 23:32:49 +0300

    ✅ Done

--- SaudiArabia/SaudiArabia/settings.py
+++ SaudiArabia/SaudiArabia/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django_browser_reload",
     'main',
 ]
 
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'SaudiArabia.urls'
